8da7e8d644f435d69855fa8961e62257dc3f113e
Vladimir Chernitsyn <flunt1k@yandex-team.ru>
2022-06-14 12:41:31 +0300

    AUCTION-1: add connection to db

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import http from 'http';
 import app from './app';
 import SECRETS from './secrets';
+import {initDataBase} from './db';
 
 const server = http.createServer(app);
 
-server.listen(SECRETS.SERVER_PORT, () => {
+server.listen(SECRETS.SERVER_PORT, async () => {
+    await initDataBase();
     console.log(`Server has been started and listen port: ${SECRETS.SERVER_PORT}`);
 });
--- secrets/index.ts
+++ secrets/index.ts
@@ -5,14 +5,20 @@ dotenv.config();
 type ServerAppSecrets = {
     DB_HOSTNAME: string;
     DB_PASSWORD: string;
-    DB_PATH: string;
+    DB_PORT: number;
+    DB_USER: string;
+    DB_NAME: string;
+    SERT_PATH: string;
     SERVER_PORT: number | string;
 };
 
 const SECRETS: ServerAppSecrets = {
     DB_HOSTNAME: process.env.DB_HOSTNAME || '',
     DB_PASSWORD: process.env.DB_PASSWORD || '',
-    DB_PATH: process.env.DB_PATH || '',
+    DB_PORT: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3001,
+    DB_USER: process.env.DB_USER || '',
+    DB_NAME: process.env.DB_NAME || '',
+    SERT_PATH: process.env.SERT_PATH || '',
     SERVER_PORT: process.env.SERVER_PORT || 3000,
 };
 
