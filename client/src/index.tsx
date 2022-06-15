2e0142681d0ab780979ab2ee496d814a18c6ff5c
Flunt1k <chvp00@gmail.com>
2022-06-16 01:52:38 +0300

    install chakra

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ChakraProvider, extendTheme} from '@chakra-ui/react';
 import {App} from './containers/App/App';
 
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
+
+const theme = extendTheme({config});
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+    <ChakraProvider theme={theme}>
+        <App />
+    </ChakraProvider>,
+);
