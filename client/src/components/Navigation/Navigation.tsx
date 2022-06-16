36c11573ed21b0f4ee357289af6d3d8686195587
Flunt1k <chvp00@gmail.com>
2022-06-17 01:53:21 +0300

    AUCTION-7 enhance code

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,57 @@
 import React from 'react';
 import {Box, Button, Center, Flex, Icon, IconButton, Spacer} from '@chakra-ui/react';
-import {FaHome} from 'react-icons/fa';
 import {RiAuctionFill} from 'react-icons/ri';
-import {MdSell} from 'react-icons/md';
 import {BsArrowRightSquare} from 'react-icons/bs';
+import {IconType} from 'react-icons';
 
-export const Navigation: React.FC = () => {
+export type AsideMenuConfig = {
+    openText: string;
+    icon: IconType;
+    aria: string;
+};
+
+type MenuButtonProps = {
+    isOpen: boolean;
+    icon: IconType;
+    buttonText: string;
+    aria: string;
+};
+
+export const MenuButton: React.FC<MenuButtonProps> = ({isOpen, buttonText, icon, aria}) => {
+    return isOpen ? (
+        <Button
+            variant="ghost"
+            borderRadius="0"
+            aria-label={aria}
+            borderBottom="1px solid white"
+            width="100%"
+        >
+            <Flex width="100%">
+                <Center>
+                    <Icon as={icon} marginRight="5px" />
+                    {buttonText}
+                </Center>
+            </Flex>
+        </Button>
+    ) : (
+        <Center>
+            <IconButton
+                borderBottom="1px solid white"
+                variant="ghost"
+                borderRadius="0"
+                aria-label={aria}
+                icon={<Icon as={icon} />}
+                width="100%"
+            />
+        </Center>
+    );
+};
+
+type NavigationProps = {
+    config: AsideMenuConfig[];
+};
+
+export const Navigation: React.FC<NavigationProps> = ({config}) => {
     const [isOpen, setIsOpen] = React.useState(false);
 
     React.useEffect(() => {
@@ -31,56 +77,17 @@ export const Navigation: React.FC = () => {
                         _hover={{cursor: 'pointer'}}
                     />
                 </Center>
-                <Box borderTop="1px solid white" borderBottom="1px solid white" marginTop="20px">
-                    <Center>
-                        {isOpen ? (
-                            <Button
-                                variant="ghost"
-                                borderRadius="0"
-                                aria-label={'user'}
-                                borderBottom="1px solid white"
-                            >
-                                <Flex>
-                                    <Center>
-                                        <Icon as={FaHome} marginRight="5px" />
-                                        Домашняя страница
-                                    </Center>
-                                </Flex>
-                            </Button>
-                        ) : (
-                            <IconButton
-                                borderBottom="1px solid white"
-                                variant="ghost"
-                                borderRadius="0"
-                                aria-label={'user'}
-                                icon={<Icon as={FaHome} />}
-                                width="100%"
+                <Box borderTop="1px solid white" marginTop="20px">
+                    {config.map(({openText, icon, aria}) => {
+                        return (
+                            <MenuButton
+                                isOpen={isOpen}
+                                icon={icon}
+                                buttonText={openText}
+                                aria={aria}
                             />
-                        )}
-                    </Center>
-
-                    {isOpen ? (
-                        <Button variant="ghost" borderRadius="0" aria-label={'user'} width="100%">
-                            <Flex width="100%">
-                                <Box justifyContent="flex-start" alignItems="center">
-                                    <Center>
-                                        <Icon as={MdSell} marginRight="5px" />
-                                        Продажи
-                                    </Center>
-                                </Box>
-                            </Flex>
-                        </Button>
-                    ) : (
-                        <Center>
-                            <IconButton
-                                variant="ghost"
-                                borderRadius="0"
-                                aria-label={'user'}
-                                icon={<Icon as={MdSell} />}
-                                width="100%"
-                            />
-                        </Center>
-                    )}
+                        );
+                    })}
                 </Box>
                 <Spacer />
                 <Center>
--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import {Box, Flex} from '@chakra-ui/react';
-import {Navigation} from '../../components/Navigation/Navigation';
+import {FaHome} from 'react-icons/fa';
+import {MdSell} from 'react-icons/md';
+import {AsideMenuConfig, Navigation} from '../../components/Navigation/Navigation';
+
+const ASIDE_MENU_BUTTONS: AsideMenuConfig[] = [
+    {
+        openText: 'Домашняя страница',
+        icon: FaHome,
+        aria: 'home-page',
+    },
+    {
+        openText: 'Каталог товаров',
+        icon: MdSell,
+        aria: 'sell-page',
+    },
+];
 
 export const App = () => {
     return (
         <Flex height="100vh" width="100vw" overflow="hidden">
             <Box height="100vh" flex="0 0 auto">
-                <Navigation />
+                <Navigation config={ASIDE_MENU_BUTTONS} />
             </Box>
             <Box height="100vh" flex="1"></Box>
         </Flex>
