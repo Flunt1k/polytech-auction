b3205aed9c9cc0b61b89fcce9b63802d1460c03d
Mikhail Borisov <mihbor51@icloud.com>
2022-06-17 04:34:28 +0300

    auth form added

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -3,6 +3,7 @@ import {Box, Flex} from '@chakra-ui/react';
 import {FaHome} from 'react-icons/fa';
 import {MdSell} from 'react-icons/md';
 import {AsideMenuConfig, Navigation} from '../../components/Navigation/Navigation';
+import AuthPage from '../../pages/auth';
 
 const ASIDE_MENU_BUTTONS: AsideMenuConfig[] = [
     {
@@ -20,10 +21,7 @@ const ASIDE_MENU_BUTTONS: AsideMenuConfig[] = [
 export const App = () => {
     return (
         <Flex height="100vh" width="100vw" overflow="hidden">
-            <Box height="100vh" flex="0 0 auto">
-                <Navigation config={ASIDE_MENU_BUTTONS} />
-            </Box>
-            <Box height="100vh" flex="1"></Box>
+            <AuthPage />
         </Flex>
     );
 };
---
+++ client/src/pages/auth/index.tsx
@@ -0,0 +1,63 @@
+import React, {useEffect} from 'react';
+import {Input, Button, FormErrorMessage, FormControl} from '@chakra-ui/react';
+import {useForm} from 'react-hook-form';
+
+const AuthPage = () => {
+    const {
+        handleSubmit,
+        register,
+        formState: {errors, isSubmitting},
+    } = useForm({
+        mode: 'onSubmit',
+        reValidateMode: 'onChange',
+        shouldFocusError: true,
+    });
+
+    const onSubmit = () => {
+        return new Promise<void>((resolve) => {
+            setTimeout(() => {
+                console.log('submit');
+
+                resolve();
+            }, 3000);
+        });
+    };
+
+    useEffect(() => {
+        console.log(errors);
+    }, [errors?.password]);
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <FormControl isInvalid={errors?.email}>
+                <Input
+                    isInvalid={errors?.email}
+                    type="email"
+                    placeholder="email"
+                    {...register('email', {
+                        required: 'Пожалуйста, введите Email',
+                        pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                    })}
+                />
+            </FormControl>
+            <FormControl>
+                <Input
+                    isInvalid={errors?.password}
+                    type="password"
+                    placeholder="password"
+                    {...register('password', {
+                        required: 'Пожалуйста, введите пароль',
+                        minLength: {value: 6, message: 'Пароль должен быть больше 6-ти символов'},
+                        maxLength: {value: 10, message: 'Пароль должен быть меньше 10-ти символов'},
+                    })}
+                />
+                <p>{errors?.password?.message || null}</p>
+            </FormControl>
+            <Button colorScheme="teal" isLoading={isSubmitting} type="submit">
+                Submit
+            </Button>
+        </form>
+    );
+};
+
+export default AuthPage;
