0f399efdc7cc8a10ed70bbf41542e72d1cb3c0f7
Mikhail Borisov <66256831+asinistr51@users.noreply.github.com>
2022-06-18 00:59:31 +0300

    Feature/auth router (#15)

--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -2,27 +2,46 @@ import React from 'react';
 import {Box, Flex} from '@chakra-ui/react';
 import {FaHome} from 'react-icons/fa';
 import {MdSell} from 'react-icons/md';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate, useLocation} from 'react-router-dom';
 import {AsideMenuConfig, Navigation} from '../../components/Navigation/Navigation';
+// import {ProductCard} from '../../components/ProductCard/ProductCard';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchAllProducts} from '../../redux/products/actions';
+import {AppDispatch} from '../../redux/store';
+import {decodeJwt} from '../../utils/jwt';
 
 import AuthPage from '../../pages/auth';
 import RegPage from '../../pages/reg';
 import GalleryPage from '../../pages/gallery';
 import ProductPage from '../../pages/product';
 import HomePage from '../../pages/home';
+import {selectToken} from '../../redux/user/selectors';
 
 const ASIDE_MENU_BUTTONS: AsideMenuConfig[] = [
     {
         openText: 'Домашняя страница',
         icon: FaHome,
         aria: 'home-page',
+        path: '/',
     },
     {
         openText: 'Каталог товаров',
         icon: MdSell,
         aria: 'sell-page',
+        path: '/products',
     },
 ];
+
+const RequireAuth = ({children}: {children: JSX.Element}) => {
+    const token = useSelector(selectToken);
+    let location = useLocation();
+
+    if (!token || decodeJwt(token).exp < Date.now() / 1000) {
+        return <Navigate to="/login" state={{from: location}} replace />;
+    }
+
+    return children;
+};
 
 export const App = () => {
     return (
@@ -31,12 +50,29 @@ export const App = () => {
                 <Box height="100vh" flex="0 0 auto">
                     <Navigation config={ASIDE_MENU_BUTTONS} />
                 </Box>
-                <Box height="100vh" flex="1" overflow="auto"></Box>
+                <Box height="100vh" flex="1" overflow="auto">
+                    <Routes>
+                        <Route
+                            path="/"
+                            element={
+                                <RequireAuth>
+                                    <HomePage />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route
+                            path="/products"
+                            element={
+                                <RequireAuth>
+                                    <GalleryPage />
+                                </RequireAuth>
+                            }
+                        />
+                        <Route path="/login" element={<AuthPage />} />
+                        <Route path="/registration" element={<RegPage />} />
+                    </Routes>
+                </Box>
             </Flex>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Routes />
-            </Routes>
         </React.Fragment>
     );
 };
--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import {Box} from '@chakra-ui/react';
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+  return <Box>HomePage</Box>;
 }
 
 export default HomePage
