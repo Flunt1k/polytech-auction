7659d84b4b37a1813ce950775ef50c817276e981
Vladimir Chernitsyn <47900126+Flunt1k@users.noreply.github.com>
2022-06-18 03:30:22 +0300

    AUCTION-15 (#17)

--- client/src/components/Modal/CreateProductModal.tsx
+++ client/src/components/Modal/CreateProductModal.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+    Button,
     FormControl,
     FormLabel,
     Input,
@@ -7,10 +8,16 @@ import {
     ModalBody,
     ModalCloseButton,
     ModalContent,
+    ModalFooter,
     ModalHeader,
     ModalOverlay,
+    Textarea,
 } from '@chakra-ui/react';
-import {useLocation, useNavigate} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch} from '../../redux/store';
+import {createProduct} from '../../redux/products/actions';
+import {ProductCreateArgs} from '../../types';
+import {selectUser} from '../../redux/user/selectors';
 
 type Props = {
     isOpen: boolean;
@@ -18,9 +25,31 @@ type Props = {
 };
 
 export const CreateProductModal: React.FC<Props> = (props: Props) => {
+    const dispatch = useDispatch<AppDispatch>();
+
+    const user = useSelector(selectUser);
+
+    const [formState, setFormState] = React.useState<ProductCreateArgs>({
+        productName: '',
+        image: '',
+        buyInPrice: undefined,
+        initialPrice: undefined,
+        deadline: undefined,
+        description: '',
+        year: '',
+        ownerId: user?.id || '',
+    });
     const initialRef = React.useRef(null);
 
+    const handleCreateProduct = React.useCallback(() => {
+        if (formState) {
+            dispatch(createProduct(formState));
+        }
+    }, []);
+
     const {isOpen} = props;
+
+    console.log(formState);
 
     return (
         <Modal isOpen={isOpen} onClose={props.onClose} initialFocusRef={initialRef}>
@@ -30,15 +59,106 @@ export const CreateProductModal: React.FC<Props> = (props: Props) => {
                 <ModalCloseButton />
                 <ModalBody pb={6}>
                     <FormControl>
-                        <FormLabel>First name</FormLabel>
-                        <Input ref={initialRef} placeholder="First name" />
+                        <FormLabel>Название товара</FormLabel>
+                        <Input
+                            ref={initialRef}
+                            value={formState.productName}
+                            required={true}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    productName: event.target.value,
+                                }))
+                            }
+                        />
                     </FormControl>
 
                     <FormControl mt={4}>
-                        <FormLabel>Last name</FormLabel>
-                        <Input placeholder="Last name" />
+                        <FormLabel>Описание</FormLabel>
+                        <Textarea
+                            placeholder="Описание"
+                            value={formState.description}
+                            required={true}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    description: event.target.value,
+                                }))
+                            }
+                        />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                        <FormLabel>Год создания</FormLabel>
+                        <Input
+                            type="number"
+                            required={true}
+                            value={formState.year}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    year: event.target.value,
+                                }))
+                            }
+                        />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                        <FormLabel>Стартовая цена</FormLabel>
+                        <Input
+                            type="number"
+                            required={true}
+                            value={formState.initialPrice}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    initialPrice: +event.target.value,
+                                }))
+                            }
+                        />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                        <FormLabel>Цена выкупа (не обязательн)</FormLabel>
+                        <Input
+                            type="number"
+                            value={formState.buyInPrice}
+                            required={false}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    buyInPrice: +event.target.value,
+                                }))
+                            }
+                        />
+                    </FormControl>
+
+                    <FormControl mt={4}>
+                        <FormLabel>Дата окончания</FormLabel>
+                        <Input
+                            type="datetime-local"
+                            value={formState.deadline}
+                            required={false}
+                            onChange={(event) =>
+                                setFormState((prevState: ProductCreateArgs) => ({
+                                    ...prevState,
+                                    deadline: event.target.value,
+                                }))
+                            }
+                        />
                     </FormControl>
                 </ModalBody>
+                <ModalFooter>
+                    <Button
+                        colorScheme="blue"
+                        mr={3}
+                        disabled={!formState}
+                        onClick={handleCreateProduct}
+                    >
+                        Save
+                    </Button>
+                    <Button onClick={props.onClose}>Cancel</Button>
+                </ModalFooter>
             </ModalContent>
         </Modal>
     );
